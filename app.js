d4632b0bb04bba1a08f92467a8d30d8668ca4126
Casey Leonard <casey@amphibian.com>
2018-09-01 12:53:48 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+
+
+app.get('/snowdays', function(req, res, next) {
+
+	let sd = [
+		"2018-12-01"
+	];
+
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(sd));
+
+});
+
+app.use(express.static('static', {
+	dotfiles: 'ignore',
+	etag: false,
+	extensions: ['htm', 'html'],
+	maxAge: '1d',
+	redirect: false,
+	setHeaders: function (res, path, stat) {
+	  res.set('x-timestamp', Date.now())
+	}
+  })
+);
+
+// ------------------------- start the listening
+
+var server = app.listen(3500, function() {
+	console.log('listening on port %d', server.address().port);
+});
